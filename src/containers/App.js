2cd2dfbe7844c57887e04207ffb5600db5fcfa86
madanmohanw95 <madhanmohanw95@gmail.com>
2024-05-27 12:33:17 -0500

    Implemented react hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,44 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: [],
-      searchfield: '',
-    }
-  }
+const App = () => {
+  const [searchfield, setSearchfield] = useState('');
+  const [robots, setRobots] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => this.setState({ robots: users }))
-  }
+    .then((response) => response.json())
+    .then((users) => setRobots(users))
+  }, [])
 
-  onSearchChange = (event) => {
-    const searchfield = event.target.value;
-    if (searchfield !== this.state.searchfield) {
-      this.setState({ searchfield: event.target.value })
+  const onSearchChange = (event) => {
+    const searchValue = event.target.value;
+    if (searchfield !== searchValue) {
+      setSearchfield(event.target.value)
     }
   }
 
-  render() {
-    const { robots, searchfield } = this.state;
-    const filteredRobots = robots.filter(robot => {
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-    })
+  const filteredRobots = robots.filter(robot => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+  })
 
-    return !robots.length ?
+  return !robots.length ?
       <h1>Loading</h1> :
       (
         <div className='tc'>
           <h1 className='f2'>Robots</h1>
-          <SearchBox searchChange={this.onSearchChange} />
+          <SearchBox searchChange={onSearchChange} />
           <Scroll>
             <ErrorBoundary fallback="Error">
               <CardList robots={filteredRobots} />
@@ -46,7 +39,6 @@ class App extends Component {
           </Scroll>
         </div>
       )
-  }
 
 }
 
